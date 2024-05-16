f61e60076ae1e51c5f6cbd60dc43167306ec1fae
Vishalpatil512 <patilvd512@gmail.com>
2024-05-16 20:06:01 +0530

    git vishal

--- GitDemo/src/com/A.java
+++ GitDemo/src/com/A.java
@@ -1,9 +1,10 @@
 package com;
 
 public class A {
-	public 	void m1()
+	public 	void push()
 	{
 		System.out.println("M1");
+		
 	}
 	public static void main(String[] args) {
 		System.out.println("This is git demo");
