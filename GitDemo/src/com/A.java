ab74515bd5fff7b500d3239ec1f54f1db1c3c9fd
Vishalpatil512 <patilvd512@gmail.com>
2024-05-17 08:55:25 +0530

    xyz

--- GitDemo/src/com/A.java
+++ GitDemo/src/com/A.java
@@ -15,6 +15,10 @@ public class A {
 	{
 		System.out.println("m4");
 	}
+	public void m6()
+	{
+		System.out.println("m4");
+	}
 		public static void main(String[] args) {
 		System.out.println("This is git demo");
 	}
